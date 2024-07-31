2f8aaae9912938114febec493e96fa7d7cb9ea52
timerlan <lstaeee@mail.ru>
2024-07-31 20:50:38 +0300

    was added model teacher , fonts , htmls

--- agregator/users/urls.py
+++ agregator/users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from users.views import *
+
+app_name = 'users'
+
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('profile/', profile, name='profile'),
     path('', home, name='home'),
+    path('teachers/',teachers, name='teachers'),
+    path('meetings/', meetings, name='meetings'),
+    path('search/', search_teachers, name='search_teachers'),
 ]
--- agregator/users/views.py
+++ agregator/users/views.py
@@ -2,13 +2,28 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from django.http import HttpRequest, HttpResponse
+from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, StudyDirectionForm
 import requests
-from .models import CustomUser
+from .models import CustomUser, Teacher
+
+
 def home(request):
-    return render(request, 'users/base.html')
+    return render(request, 'users/home_page.html')
+
+def teachers(request):
+    context = {
+        'teachers': Teacher.objects.all()
+    }
+    return render(request, 'users/teachers.html', context)
+
+
+def meetings(request):
+    return render(request, 'users/meetings.html')
+
+
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
@@ -54,4 +69,30 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+import logging
 
+logger = logging.getLogger(__name__)
+
+
+def search_teachers(request):
+    query = request.GET.get('q', '')
+    logger.debug(f'Received query: {query}')  # Log the received query
+
+    if len(query) < 3:
+        return JsonResponse([], safe=False)
+
+    teachers = Teacher.objects.filter(name__icontains=query) | Teacher.objects.filter(surname__icontains=query)
+    logger.debug(f'Found teachers: {teachers}')  # Log the found teachers
+
+    teacher_list = [
+        {
+            'name': teacher.name,
+            'surname': teacher.surname,
+            'characteristic': teacher.characteristic,
+        }
+        for teacher in teachers
+    ]
+
+    logger.debug(f'Returning teacher list: {teacher_list}')  # Log the response data
+
+    return JsonResponse(teacher_list, safe=False)
